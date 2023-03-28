fa77721311a75210395024f262861bbe09b0a5b1
hassansaif-0 <hassansaifraja102@gmail.com>
2023-03-28 10:40:53 +0500

    ALLADDED

---
+++ main.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import numpy as np
+
+X_train_flattened = X_train.reshape(len(X_train), 28*28)
+X_test_flattened = X_test.reshape(len(X_test), 28*28)
+
+model = keras.Sequential([
+    keras.layers.Dense(10, input_shape=(784,), activation='sigmoid')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(X_train_flattened, y_train, epochs=2)
+
+model.evaluate(X_test_flattened, y_test)
+y_predicted = model.predict(X_test_flattened)
+y_predicted[0]
+
+y_predicted_labels = [np.argmax(i) for i in y_predicted]
